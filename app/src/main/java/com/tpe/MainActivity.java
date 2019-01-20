ee082746ef58cbc4dc01fa059bd35b0ee28e8725
Corentin <coco.hervaud@gmail.com>
2019-01-20 21:18:50 +0100

    premières frames

--- app/src/main/java/com/tpe/MainActivity.java
+++ app/src/main/java/com/tpe/MainActivity.java
@@ -5,23 +5,32 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    private FrameLayout OnOff;
+    private FrameLayout Parasol;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            OnOff.setVisibility(FrameLayout.INVISIBLE);
+            Parasol.setVisibility(FrameLayout.INVISIBLE);
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    OnOff.setVisibility(FrameLayout.VISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    Parasol.setVisibility(FrameLayout.VISIBLE);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -35,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OnOff = findViewById(R.id.OnOff);
+        Parasol = findViewById(R.id.Parasol);
+        OnOff.setVisibility(FrameLayout.VISIBLE);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
