52687d5c389dad2b635ac820dd872460e30257c3
Corentin <coco.hervaud@gmail.com>
2019-01-23 20:35:17 +0100

    Navig_icons

--- app/src/main/java/com/tpe/MainActivity.java
+++ app/src/main/java/com/tpe/MainActivity.java
@@ -5,13 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mTextMessage;
-
     private FrameLayout OnOff;
     private FrameLayout Parasol;
     private FrameLayout Ombre;
@@ -28,20 +30,20 @@ public class MainActivity extends AppCompatActivity {
             Infos.setVisibility(FrameLayout.INVISIBLE);
 
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.power_home:
+                    mTextMessage.setText("Power");
                     OnOff.setVisibility(FrameLayout.VISIBLE);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.parasol_home:
+                    mTextMessage.setText("Parasol");
                     Parasol.setVisibility(FrameLayout.VISIBLE);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.ombre_home:
+                    mTextMessage.setText("Ombre");
                     Ombre.setVisibility(FrameLayout.VISIBLE);
                     return true;
-                case R.id.navigation_other:
-                    mTextMessage.setText("other");
+                case R.id.infos_home:
+                    mTextMessage.setText("Infos");
                     Infos.setVisibility(FrameLayout.VISIBLE);
                     return true;
             }
@@ -60,9 +62,29 @@ public class MainActivity extends AppCompatActivity {
         Infos = findViewById(R.id.Infos);
         OnOff.setVisibility(FrameLayout.VISIBLE);
 
+        ImageView ButOnclick = (ImageView) findViewById(R.id.ButOn);
+        ButOnclick.setOnClickListener(this);
+        ImageView ButOffclick = (ImageView) findViewById(R.id.ButOff);
+        ButOffclick.setOnClickListener(this);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case  R.id.ButOn: {
+                mTextMessage.setText("click On");
+                break;
+            }
+            case  R.id.ButOff: {
+                mTextMessage.setText("click Off");
+                break;
+            }
+            //.... etc
+        }
+    }
 }
